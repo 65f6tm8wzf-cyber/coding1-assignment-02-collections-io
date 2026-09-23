8469d4d4a78eb55bace20667c8134835cae4fed2
Mishel Voskanyan <voskanyan_mishel@student.edu.com>
2026-09-23 17:16:46 +0200

    I tried to explain what each line of code does by adding comments next to each line, as well as I tried to predict what will be the output of the print functions, which I did correctly :D)

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea" #this will assing the value tea to the drink variable 
+sales = {"tea": 3, "coffee": 5} #tells us how many units of tea and coffee have been sold 
+sold = sales[drink] #this will assign the number of teas sold
+sold = sold + 1 #this will increase the value of sold by 1
+print(drink) #will print tea
+print(sold) #will print 4
